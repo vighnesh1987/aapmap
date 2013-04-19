1fef995ef9e1f3be12a477966cb3b5bd26c21f84
Vighnesh Rege <vighnesh1987@gmail.com>
2013-04-18 23:09:43 -0700

    frst commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aapmap::Application.config.secret_token = 'a93f9ea606ddae807866a083b5cf69c679ae4b0feda74fc5eacb55d8bd69e37d2eaf0649bcf0e91cd2e75545cf67f75adfad88c40c749379ca97fb9fb9c250d7'
